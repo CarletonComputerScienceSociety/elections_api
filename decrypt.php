0ef8b90ad92d7f2b68db159adfe488cfcdf384fb
Forest Anderson <forestkzanderson@gmail.com>
2019-03-12 18:47:51 +0100

    Main for testing

--- decrypt.php
+++ decrypt.php
@@ -1,7 +1,7 @@
 <?php
 $key = $argv[1];
 $iv = $argv[2];
-$enc = $argv[3];
+$enc = trim(fgets(STDIN));
 
 $plaintext=openssl_decrypt($enc, "aes-256-cbc", $key, $options=0, $iv);
 echo $plaintext
---
+++ generate_env.py
@@ -0,0 +1,8 @@
+import os, random, string
+
+out_text = "# The shared key given by the SCS must be entered here\nCCSS_SHARED_KEY=\n\n# The IV given by the SCS must be entered here\nCCSS_IV=\n\n"
+out_text += "SECRET_KEY=" + str(''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(64))
+)
+
+with open(".env", "w") as text_file:
+    text_file.write(out_text)
