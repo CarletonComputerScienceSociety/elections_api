1401b73eafe2ca876fd3f95af7efc0e2c15ba774
Forest Anderson <forestkzanderson@gmail.com>
2019-03-25 23:45:38 -0400

    Fixed server problems

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 # FLASK
-from flask import Flask, request
+from flask import Flask, request, status
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
@@ -33,7 +33,11 @@ def vote():
     try:
         request_data = json.loads(request.data)
     except:
-        return [-1, "Error, invalid json"]
+        return "Error, invalid json", HTTP_400_BAD_REQUEST
+    
+    if('scs_key' not in request_data or request_data['scs_key'] == None):
+        print("THE SCS_KEY IS NULL OR INVALID")
+        return "Error, scs_key is required", HTTP_400_BAD_REQUEST
 
     scs_key_url_encrypted = request_data['scs_key']
     client_vote = request_data['vote']
@@ -46,8 +50,10 @@ def vote():
     # The key is sent url encrypted by the SCS, so it needs to be decoded
     scs_encrypted_key = urllib.parse.unquote(scs_key_url_encrypted)
 
+
     # The decryption is done in a subprocess in PHP because
     # decryption is not working in Python
+
     scs_decryption_process = subprocess.run(
         ['php', 'util/decrypt.php', CCSS_SHARED_KEY, CCSS_IV],
         stdout=subprocess.PIPE,                     # The output from the PHP script
@@ -56,9 +62,14 @@ def vote():
     )
     
     separated = str(scs_decryption_process.stdout).split(" ")
-    time = separated[0].split("=")[1]
-    user = separated[1].split("=")[1]
-    ip = separated[2].split("=")[1]
+
+    #catch invalid ciphertext that passed decryption
+    try:
+        time = separated[0].split("=")[1]
+        user = separated[1].split("=")[1]
+        ip = separated[2].split("=")[1]
+    except:
+        return "Error, invalid ciphertext", HTTP_400_BAD_REQUEST
 
     # If we get this far without an error, then we know that the user's
     # ciphertext is valid, and was encrypted by the SCS.
@@ -76,11 +87,11 @@ def vote():
         })
 
         if (len(duplicate_user_votes) > 0):
-            return "You have already voted! We deleted your old vote and replaced it with this one."
+            return "You have already voted! We deleted your old vote and replaced it with this one.", HTTP_200_OK
         else:
-            return "Your vote was recorded! Thanks so much for voting in the CCSS general elections!"
+            return "Your vote was recorded! Thanks so much for voting in the CCSS general elections!", HTTP_200_OK
     else:
-        return result[1]
+        return result[1], HTTP_400_BAD_REQUEST
 
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
